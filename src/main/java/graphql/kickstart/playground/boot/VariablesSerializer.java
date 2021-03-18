d35c0cd0a1fb9fd9266784af03e6564bc2820727
hennihaus <jan-hendrik.hausner@outlook.com>
2021-03-18 16:50:52 +0100

    fixed NullPointerException in Serializer #patch

--- src/main/java/graphql/kickstart/playground/boot/VariablesSerializer.java
+++ src/main/java/graphql/kickstart/playground/boot/VariablesSerializer.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
@@ -13,12 +12,10 @@ import java.util.Map;
 @JsonComponent
 public class VariablesSerializer extends JsonSerializer<Map<String, Object>> {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Override
     public void serialize(Map<String, Object> map, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        String json = objectMapper.writeValueAsString(map);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(map);
         gen.writeString(json);
     }
 }
