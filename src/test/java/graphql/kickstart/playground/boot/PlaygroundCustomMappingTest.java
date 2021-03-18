9b55a1fc53415ec526c25a7ce15a80704f5d075a
hennihaus <jan-hendrik.hausner@outlook.com>
2021-03-18 10:49:30 +0100

    removed all sub projects apart of playground configuration and added maven support instead of gradle #patch

---
+++ src/test/java/graphql/kickstart/playground/boot/PlaygroundCustomMappingTest.java
@@ -0,0 +1,40 @@
+package graphql.kickstart.playground.boot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PlaygroundTestConfig.class)
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-playground-mapping-test.properties")
+class PlaygroundCustomMappingTest {
+
+  private static final String CONFIGURED_MAPPING = "/test-mapping";
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void shouldUseTheConfiguredRequestMapping() throws Exception {
+    mockMvc.perform(get(CONFIGURED_MAPPING))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+        .andExpect(content().encoding(StandardCharsets.UTF_8.name()))
+        .andExpect(content().string(not(isEmptyString())));
+  }
+}
